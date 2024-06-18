3d416be013d103ba3b9b576c7bea1122658ede5c
GavinCraft-Studios <gavincraft694@gmail.com>
2024-06-18 09:25:54 -0400

    Added Custom Font

--- pages/js/innerhtml.js
+++ pages/js/innerhtml.js
@@ -1,3 +1,3 @@
-document.getElementById("mainNav").innerHTML = '<li><a href="https://gavindb.dev">Home</a></li><li class="right"><a href="https://gavindb.dev/pages/about.html">About</a></li><li class="right"><a href="https://gavindb.dev/pages/search.html"><img src="https://gavindb.dev/media/search-icon.png"></a></li>';
+document.getElementById("mainNav").innerHTML = '<li><a href="https://gavindb.dev">Home</a></li><li class="right"><a href="https://gavindb.dev/pages/search.html"><img src="https://gavindb.dev/media/search-icon.png"></a></li><li class="right"><a href="https://gavindb.dev/pages/about.html">About</a></li>';
 let contents = document.getElementById("body").innerHTML;
 document.getElementById("body").innerHTML = '<div class="light x1"></div><div class="light x2"></div><div class="light x3"></div><div class="light x4"></div><div class="light x5"></div><div class="light x6"></div><div class="light x7"></div><div class="light x8"></div><div class="light x9"></div>' + contents;
